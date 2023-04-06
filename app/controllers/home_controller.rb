e19cd6e4aeecb5f186f1f0b91e2da1678bd89e44
khalidltf <khalidlotfi2005@gmail.com>
2023-04-06 13:07:00 +0000

    update
    Signed-off-by: khalidltf <khalidlotfi2005@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def index
+    # ...
   end
 
   def about
-    @about_me = " Hi ya! My Name is Bold ...."
+    # ...
   end
 end
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,6 @@
 class Friend < ApplicationRecord
+  validates :first_name, :last_name, :email, :phone, :twitter, presence: true
+  # validates_presence_of :first_name, :last_name, :birthday, :home
+
   belongs_to :user
 end
